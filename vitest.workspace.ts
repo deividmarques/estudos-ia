2fbe23deda8a8bc52db3338e3689e7fd62a87f33
Deivid Marques <deividmarques@gmail.com>
2025-06-02 21:28:47 -0300

    fix: github actions

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,25 +3,6 @@ import { fileURLToPath } from 'node:url';
 
 import { defineWorkspace } from 'vitest/config';
 
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-
-const dirname =
-  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineWorkspace([
   'vite.config.ts',
-  {
-    extends: 'vite.config.ts',
-    plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({ configDir: path.join(dirname, '.storybook') }),
-    ],
-    test: {
-      name: 'storybook',
-      // Removido bloco browser para evitar dependência do Playwright no CI
-      setupFiles: ['.storybook/vitest.setup.ts'],
-    },
-  },
 ]);
